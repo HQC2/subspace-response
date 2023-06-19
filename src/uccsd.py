0f0a9dfc222e8f3ed881242cb04fbbdefe4b5e76
Peter Reinholdt <peter.reinholdt@gmail.com>
2023-06-19 20:46:17 +0200

    Use pyscf as backend for MO dipole integrals

--- src/uccsd.py
+++ src/uccsd.py
@@ -9,9 +9,13 @@ from scipy.optimize import minimize
 import periodictable
 import functools
 from uccsd_exc import UCCSD_exc
+import pyscf
 
-def uccsd_ground_state(symbols, geometry, charge):
-    H, qubits = qml.qchem.molecular_hamiltonian(symbols, geometry, charge=charge, method='pyscf')
+def uccsd_ground_state(symbols, geometry, charge, basis=None):
+    if basis is None:
+        basis = 'STO-3G'
+
+    H, qubits = qml.qchem.molecular_hamiltonian(symbols, geometry, charge=charge, method='pyscf', basis=basis)
 
     electrons = sum([periodictable.elements.__dict__[symbol].number for symbol in symbols]) - charge
 
@@ -43,8 +47,10 @@ def uccsd_ground_state(symbols, geometry, charge):
 
     return theta_opt
 
-def uccsd_hvp(symbols, geometry, charge, theta_opt):
-    H, qubits = qml.qchem.molecular_hamiltonian(symbols, geometry, charge=charge, method='pyscf')
+def uccsd_hvp(symbols, geometry, charge, theta_opt, basis=None, h=1e-6):
+    if basis is None:
+        basis = 'STO-3G'
+    H, qubits = qml.qchem.molecular_hamiltonian(symbols, geometry, charge=charge, method='pyscf', basis=basis)
 
     electrons = sum([periodictable.elements.__dict__[symbol].number for symbol in symbols]) - charge
 
@@ -73,7 +79,7 @@ def uccsd_hvp(symbols, geometry, charge, theta_opt):
         grad = fg[len(theta_opt):]
         return grad
 
-    def hessian_vector_product(v, grad, theta, h=1e-6):
+    def hessian_vector_product(v, grad, theta, h):
         hvp = np.zeros_like(v)
         if len(v.shape) == 1:
             hvp = (grad(theta+h*v) - grad(theta-h*v)) / (2*h)
@@ -85,11 +91,13 @@ def uccsd_hvp(symbols, geometry, charge, theta_opt):
         return hvp
 
     dim = 1*(len(singles) + len(doubles))
-    hvp = functools.partial(hessian_vector_product, grad=jac_exc, theta=qml.numpy.zeros(dim))
+    hvp = functools.partial(hessian_vector_product, grad=jac_exc, theta=qml.numpy.zeros(dim), h=h)
     return hvp
 
-def uccsd_dipole_property_gradient(symbols, geometry, charge, theta_opt):
-    H, qubits = qml.qchem.molecular_hamiltonian(symbols, geometry, charge=charge, method='pyscf')
+def uccsd_dipole_property_gradient(symbols, geometry, charge, theta_opt, basis=None):
+    if basis is None:
+        basis = 'STO-3G'
+    H, qubits = qml.qchem.molecular_hamiltonian(symbols, geometry, charge=charge, method='pyscf', basis=basis)
 
     electrons = sum([periodictable.elements.__dict__[symbol].number for symbol in symbols]) - charge
 
@@ -98,10 +106,29 @@ def uccsd_dipole_property_gradient(symbols, geometry, charge, theta_opt):
     s_wires, d_wires = qml.qchem.excitations_to_wires(singles, doubles)
 
     dev = qml.device("lightning.qubit", wires=qubits)
-    mol = qml.qchem.Molecule(symbols, geometry)
-    dipole_x = qml.qchem.qubit_observable(qml.qchem.fermionic_observable(np.array([0.0]), qml.qchem.dipole_integrals(mol)()[1][0]))
-    dipole_y = qml.qchem.qubit_observable(qml.qchem.fermionic_observable(np.array([0.0]), qml.qchem.dipole_integrals(mol)()[1][1]))
-    dipole_z = qml.qchem.qubit_observable(qml.qchem.fermionic_observable(np.array([0.0]), qml.qchem.dipole_integrals(mol)()[1][2]))
+    atom_str = ''
+    for symbol, coord in zip(symbols, geometry):
+        atom_str += f'{symbol} {coord[0]} {coord[1]} {coord[2]}; '
+        print(atom_str)
+    m = pyscf.M(atom=atom_str, basis=basis, charge=charge, unit='bohr')
+    mf = pyscf.scf.RHF(m)
+    mf.kernel()
+    mo_dipole_integrals = np.einsum('uj,xuv,vi->xij', mf.mo_coeff, m.intor('int1e_r'), mf.mo_coeff)
+    try:
+        dipole_x = qml.qchem.qubit_observable(qml.qchem.fermionic_observable(np.array([0.0]), mo_dipole_integrals[0]))
+    except IndexError:
+        dipole_x = None
+        gradx = None
+    try:
+        dipole_y = qml.qchem.qubit_observable(qml.qchem.fermionic_observable(np.array([0.0]), mo_dipole_integrals[1]))
+    except IndexError:
+        dipole_y = None
+        grady = None
+    try:
+        dipole_z = qml.qchem.qubit_observable(qml.qchem.fermionic_observable(np.array([0.0]), mo_dipole_integrals[2]))
+    except IndexError:
+        dipole_z = None
+        gradz = None
 
     @qml.qnode(dev, diff_method="adjoint")
     def circuit_dipole_x(params, wires, s_wires, d_wires, hf_state):
@@ -118,21 +145,28 @@ def uccsd_dipole_property_gradient(symbols, geometry, charge, theta_opt):
 
     params_excitation = qml.numpy.zeros(len(theta_opt) + len(singles) + len(doubles))
     params_excitation[:len(theta_opt)] = theta_opt
-    gradx = get_gradient(circuit_dipole_x)(params_excitation, range(qubits), s_wires, d_wires, hf_state)[0]
-    grady = get_gradient(circuit_dipole_y)(params_excitation, range(qubits), s_wires, d_wires, hf_state)[0]
-    gradz = get_gradient(circuit_dipole_z)(params_excitation, range(qubits), s_wires, d_wires, hf_state)[0]
-    # we want gradients on the excitation operator part
-    gradx = gradx[len(theta_opt):]
-    grady = grady[len(theta_opt):]
-    gradz = gradz[len(theta_opt):]
+    if dipole_x:
+        print('X:', circuit_dipole_x(params_excitation, range(qubits), s_wires, d_wires, hf_state))
+        gradx = get_gradient(circuit_dipole_x)(params_excitation, range(qubits), s_wires, d_wires, hf_state)[0]
+        gradx = gradx[len(theta_opt):]
+    if dipole_y:
+        print('Y:', circuit_dipole_y(params_excitation, range(qubits), s_wires, d_wires, hf_state))
+        grady = get_gradient(circuit_dipole_y)(params_excitation, range(qubits), s_wires, d_wires, hf_state)[0]
+        grady = grady[len(theta_opt):]
+    if dipole_z:
+        print('Z:', circuit_dipole_z(params_excitation, range(qubits), s_wires, d_wires, hf_state))
+        gradz = get_gradient(circuit_dipole_z)(params_excitation, range(qubits), s_wires, d_wires, hf_state)[0]
+        gradz = gradz[len(theta_opt):]
 
     return gradx, grady, gradz
 
-def hess_diag_approximate(symbols, geometry, charge):
+def hess_diag_approximate(symbols, geometry, charge, basis=None):
+    if basis is None:
+        basis = 'STO-3G'
     electrons = sum([periodictable.elements.__dict__[symbol].number for symbol in symbols]) - charge
-    H, qubits = qml.qchem.molecular_hamiltonian(symbols, geometry, charge=charge, method='pyscf')
+    H, qubits = qml.qchem.molecular_hamiltonian(symbols, geometry, charge=charge, method='pyscf', basis=basis)
     singles, doubles = qml.qchem.excitations(electrons, qubits)
-    mol = qml.qchem.Molecule(symbols, geometry)
+    mol = qml.qchem.Molecule(symbols, geometry, basis_name=basis, charge=charge)
     scf = qml.qchem.hartree_fock.scf(mol)
     orbital_energies = scf()[0]
     orbital_energies, singles, doubles
