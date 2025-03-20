8b390073c9b23a897a3ea91651d42e2100b43a3c
Peter Reinholdt <peter.reinholdt@gmail.com>
2025-03-20 13:58:03 +0100

    More work on TPA

--- src/tpa.py
+++ src/tpa.py
@@ -18,39 +18,57 @@ basis = 'STO-3G'
 
 ucc = uccsd.uccsd(symbols, geometry, charge, basis)
 ucc.ground_state()
+hdiag = ucc.hess_diag_approximate()
+
+# solve for omega and excitation vectors
+roots = 1
+omega, X = solvers.davidson_liu(ucc.hvp, hdiag, roots)
 
 # davidson_response returns (respose, history)
 intx, inty, intz = ucc.m.intor('int1e_r')
-def tpa_term(ucc, A, B, roots):
-    # solve for excited states
-    hdiag = ucc.hess_diag_approximate()
-    omega, X = solvers.davidson_liu(ucc.hvp, hdiag, roots, tol=1e-4)
-    
-    # degenerate TPA (omega_1 = omega_f/2), solve Na(omega_f - omega_1) and Nb(-omega_1) 
+def S0f_transition_moment(ucc, A, B, omega_f, Xf):
     V_A = ucc.property_gradient(A)
     V_B = ucc.property_gradient(B)
-    zero = np.zeros(len(hdiag))
-    for f in range(roots):
-        omega_f = omega[f]
-        omega_1 = omega_f/2
-        Xf = X[:,f] # normalize according to full eigenvalue problem
 
-        N_a = solvers.davidson_response(ucc.hvp, V_A, hdiag, verbose=False, omega=omega_f-omega_1)[0].real
-        N_b = solvers.davidson_response(ucc.hvp, V_B, hdiag, verbose=False, omega=-omega_1)[0].real
-        N_a_minus = solvers.davidson_response(ucc.hvp, -V_A, hdiag, verbose=False, omega=-(omega_f-omega_1))[0].real
-        N_b_minus = solvers.davidson_response(ucc.hvp, -V_B, hdiag, verbose=False, omega=-(-omega_1))[0].real
+    omega_1 = omega_f / 2
+    zero = np.zeros_like(Xf)
 
-        V2_NaBX = -ucc.V2_contraction(B, N_a, N_a_minus, Xf, zero) 
-        V2_NbAX = 0.5*(ucc.V2_contraction(A, N_b_minus, N_b, Xf, zero) + ucc.V2_contraction(A, Xf, zero, N_b_minus, N_b))
+    
+    history = None
+    N_a, history = solvers.davidson_response(ucc.hvp, V_A, hdiag, verbose=False, history=history, omega=omega_f-omega_1)
+    N_b, history = solvers.davidson_response(ucc.hvp, V_B, hdiag, verbose=False, history=history, omega=-omega_1)
+    N_a_minus, history = solvers.davidson_response(ucc.hvp, -V_A, hdiag, verbose=False, history=history, omega=-(omega_f-omega_1))
+    N_b_minus, history = solvers.davidson_response(ucc.hvp, -V_B, hdiag, verbose=False, history=history, omega=-(-omega_1))
 
-        E3_NaNbX = ucc.E3_contraction(N_a, N_a_minus, N_b_minus, N_b, Xf, zero)
-        E3_NaXNb = ucc.E3_contraction(N_a, N_a_minus, Xf, zero, N_b_minus, N_b)
+    V2_NaBX = -ucc.V2_contraction(B, N_a, N_a_minus, Xf, zero) 
+    V2_NbAX = 0.5*(ucc.V2_contraction(A, N_b_minus, N_b, Xf, zero) + ucc.V2_contraction(A, Xf, zero, N_b_minus, N_b))
 
-        S3_NaNbX = ucc.S3_contraction(N_a, N_a_minus, N_b_minus, N_b, Xf, zero)
-        S3_NaXNb = ucc.S3_contraction(N_a, N_a_minus, Xf, zero, N_b_minus, N_b)
+    E3_NaNbX = ucc.E3_contraction(N_a, N_a_minus, N_b_minus, N_b, Xf, zero)
+    E3_NaXNb = ucc.E3_contraction(N_a, N_a_minus, Xf, zero, N_b_minus, N_b)
 
-        S = V2_NaBX + V2_NbAX + E3_NaNbX + E3_NaXNb - omega_1*S3_NaNbX + omega_f*S3_NaXNb
-        print(V2_NaBX , V2_NbAX , E3_NaNbX , E3_NaXNb, - omega_1*S3_NaNbX, - omega_f*S3_NaXNb)
-        print(f'Root {f+1} omega/2={omega_f/2} S={S}')
+    S3_NaNbX = ucc.S3_contraction(N_a, N_a_minus, N_b_minus, N_b, Xf, zero)
+    S3_NaXNb = ucc.S3_contraction(N_a, N_a_minus, Xf, zero, N_b_minus, N_b)
 
-print(tpa_term(ucc, intz, intz, 3))
+    S = V2_NaBX + V2_NbAX + E3_NaNbX + E3_NaXNb - omega_1*S3_NaNbX + omega_f*S3_NaXNb
+    return S
+
+# loop over excited states
+for root in range(roots):
+    omega_f = omega[root]
+    Xf = X[:,root]
+
+    S = np.zeros((3,3))
+    S[0,0] = S0f_transition_moment(ucc, intx, intx, omega_f, Xf)
+    S[0,1] = S[1,0] = S0f_transition_moment(ucc, intx, inty, omega_f, Xf)
+    S[0,2] = S[2,0] =  S0f_transition_moment(ucc, intx, intz, omega_f, Xf)
+    S[1,1] = S0f_transition_moment(ucc, inty, inty, omega_f, Xf)
+    S[1,2] = S[2,1] = S0f_transition_moment(ucc, inty, intz, omega_f, Xf)
+    S[2,2] = S0f_transition_moment(ucc, intz, intz, omega_f, Xf)
+
+    Df = np.einsum('ii,jj->', S, S)/30.0
+    Dg = np.einsum('ij,ij->', S, S)/30.0
+
+    D_lin = 2*Df + 4*Dg
+    D_circ = -2*Df + 6*Dg
+
+    print(f'Root {root+1} {omega_f=} {S[0,0]=} {S[1,1]=} {S[2,2]=} {S[0,1]=} {S[0,2]=} {S[1,2]=} {Df=} {Dg=} {D_lin=} {D_circ=}')
